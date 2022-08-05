4a23edaef640ddb4f2db3b292ddf33b75b305d25
liangxin1988 <jay_111@163.com>
2022-08-05 11:42:26 +0800

    增加库项目，并进行maven相关配置

--- App/src/androidTest/java/ink/liangxin/demoutils/ExampleInstrumentedTest.kt
+++ App/src/androidTest/java/ink/liangxin/demoutils/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ink.liangxin.demoutils.test", appContext.packageName)
+        assertEquals("ink.liangxin.demoutils", appContext.packageName)
     }
 }
